b303b314a43e2aa61b38734d8c4e880b9fb3ab9d
Martin Millnert <martin@millnert.se>
2016-01-24 19:27:34 +0100

    Add setup.py and repackage as module
    Following simple guides of:
     - http://www.learnpython.org/en/Modules_and_Packages
     - http://mrtopf.de/blog/en/a-small-introduction-to-python-eggs/
    
    and the official documentation at:
     - https://pythonhosted.org/an_example_pypi_project/setuptools.html
    
    as well as the list of classifiers from:
     - https://pypi.python.org/pypi?%3Aaction=list_classifiers
    
    this is a module packaging and addition of setup.py for distribution.

---
+++ setup.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+""" setup.py for yamlenc - a simple YAML-based Regular Expression Puppet ENC """
+from setuptools import setup
+
+setup(
+    name='yamlenc',
+    version='0.1',
+    description='Simple YAML-based Regular Expression Puppet ENC',
+    author='IPnett AS',
+    author_email='info@cloud.ipnett.com',
+    url='https://github.com/IPnett/puppet-yaml-enc',
+    packages=['yamlenc'],
+    long_description="""\
+yamlenc is a simple YAML-based Regular Expression Puppet ENC
+(External Node Classifier).
+
+The user supplies a YAML-file (examples included) where regular expression
+based host name patterns form the key. The output is then consumed by a
+puppet master to perform external node classification for a calling node.
+""",
+    classifiers=[
+        "License :: OSI Approved :: BSD License",
+        "Programming Language :: Python",
+        "Development Status :: 3 - Alpha",
+        "Environment :: Console",
+        "Intended Audience :: System Administrators",
+        "Topic :: System :: Systems Administration",
+    ],
+    keywords='yaml puppet external node classifier enc regular expression'\
+             'regex re',
+    license='BSD',
+    install_requires=[
+        'setuptools',
+        'yaml',
+    ],
+)
---
+++ yamlenc/__init__.py
@@ -0,0 +1,3 @@
+""" Init method for yamlenc; exporting yamlenc as our API """
+#from yamlenc import YamlENC
+__all__ = ['yamlenc']
